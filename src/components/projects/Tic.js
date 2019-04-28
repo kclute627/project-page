4dbf375970ae761ab0ca50bf7124dc2682c59980
Kyle C <kyclutter@gmail.com>
2019-04-28 18:30:34 -0500

    updates

--- src/components/projects/Tic.js
+++ src/components/projects/Tic.js
@@ -1,18 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 
 
 
 class Tic extends Component {
 
+    state = {
+        startPage: false,
+        gameMap: [  
+                    ['','',''],
+                    ['','',''],
+                    ['','','']
+                ],
+        player1Name: '',
+        player2Name: '',
+
+    }
+
+    playerNameHandler = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+    submitHandler = () => {
+        this.setState({
+            startPage: false,
+        })
+    }
+
     render(){
 
-
-        return(
-            <div className = "tic">
-                <header className='tic__header'>Play Tic Tack Toe</header>
-                 
-                <div className="tic__score">
+        const { startPage, gameMap, player1Name, player2Name } = this.state;
+        
+        const mainGame = (
+            <Fragment>
+                 <div className="tic__score">
                     <div className="tic__score-top">
                         <div className="tic__score-top-1">
                             <p className="tic__score-top-1-text"></p>
@@ -52,16 +74,58 @@ class Tic extends Component {
                 
                 <div className ="tic__players">
                     <div className ="tic__players-1">
-                    Player 1
-                    <div>X</div>
+                    {player1Name ? player1Name : 'Player 1'}
+                    <p>X</p>
                     </div>
                     <div className ="tic__players-2">
-                    Player 2
-                    <div>O</div>
+                    {player2Name ? player2Name : "Player 2"}
+                    <p>O</p>
                     </div>
 
 
                 </div>
+            </Fragment>
+            
+        )
+
+        const signIn = (
+            <Fragment>
+                <div className="signin">
+                    <h1 className='signin__header'>Let's Play Please Enter Players Names</h1>
+                    <div className="signin__content">
+                        <div className="signin__player">
+                            <p>Player 1 Name</p>
+                            <input
+                            type='text'
+                            name= 'player1Name'
+                            placeholder="Player 1 Name"
+                            onChange = {this.playerNameHandler} />
+                        </div>
+                        <div className="signin__player">
+                            <p>Player 2 Name</p>
+                            <input
+                            type='text'
+                            name= 'player2Name'
+                            placeholder="Player 2 Name"
+                            onChange = {this.playerNameHandler} />
+                        </div>
+                        <button
+                        onClick={this.submitHandler}>Submit</button>
+                        
+
+                    </div>
+                </div>
+            </Fragment>
+
+        )
+
+            
+            return(
+            <div className = "tic">
+                <header className='tic__header'>Play Tic Tack Toe</header>
+                {startPage ? signIn : mainGame}
+                 
+               
                 
             </div>
         )
