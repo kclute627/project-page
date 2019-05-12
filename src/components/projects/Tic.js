346ba6e554abd00a2183d45f4ff84a641717e5b7
Kyle C <kyclutter@gmail.com>
2019-05-12 11:13:37 -0500

    updates

--- src/components/projects/Tic.js
+++ src/components/projects/Tic.js
@@ -7,6 +7,7 @@ class Tic extends Component {
 
     state = {
         startPage: false,
+        
         gameMap: [  
                     ['','',''],
                     ['','',''],
@@ -14,7 +15,9 @@ class Tic extends Component {
                 ],
         player1Name: '',
         player2Name: '',
-        currentPlayer: 'player2'
+        currentPlayer: 'player1',
+        winner: false,
+        catGame: false,
 
     }
 
@@ -59,35 +62,143 @@ class Tic extends Component {
                 gameMap: [...newGameMap]
             })
         }
-        this.switchUser();
+        if(this.checkWinner()){
+           this.checkWinner(); 
+        }else if (!this.state.catGame){
+            this.catGameHandler()
+            this.switchUser();
+        }
+          
     }
 
-    // check for a winner after every turn 
+    // check for a winner after every turn
+
+    checkWinner = ()=>{
+        const {gameMap} = this.state;
+        if(
+            //across
+            (gameMap[0][0] && gameMap[0][0] === gameMap[0][1] && gameMap[0][2] === gameMap[0][0])
+            ||
+            (gameMap[1][0] && gameMap[1][0] === gameMap[1][1] && gameMap[1][2] === gameMap[1][0])
+            ||
+            (gameMap[2][0] && gameMap[2][0] === gameMap[2][1] && gameMap[2][2] === gameMap[2][0])
+            ){
+            this.setState({
+                winner: true
+            })
+           return true;
+        }else if(
+            //down
+            (gameMap[0][0] && gameMap[0][0] === gameMap[1][0] && gameMap[2][0] === gameMap[0][0])
+            ||
+            (gameMap[0][1] && gameMap[0][1] === gameMap[1][1] && gameMap[2][1] === gameMap[0][1])
+            ||
+            (gameMap[0][2] && gameMap[0][2] === gameMap[1][2] && gameMap[2][2] === gameMap[0][2])
+        ){
+            this.setState({
+                winner: true
+            })
+           return true;
+
+        }else if(
+            //across
+            (gameMap[0][0] && gameMap[0][0] === gameMap[1][1] && gameMap[2][2] === gameMap[0][0])
+            ||
+            (gameMap[0][2] && gameMap[0][2] === gameMap[1][1] && gameMap[2][0] === gameMap[0][2])
+        ){
+            this.setState({
+                winner: true
+            })
+           return true;         
+            
+        }else{
+            return false;
+        }
+    }
+
+    catGameHandler = ()=> {
+        const { gameMap, winner } = this.state;
+
+        let cat = gameMap[0].join('')
+        cat = cat + gameMap[1].join('')
+        cat = cat +gameMap[2].join('');
+
+        if(!winner && cat.length === 9){
+            this.setState({
+                catGame: true,
+            })
+        }
+    }
+
+    restartHandler = () => {
+
+        let curPlayer = this.state.currentPlayer;
+
+        
+        if (curPlayer === 'player1'){
+            curPlayer = 'player2'
+        }else{
+            curPlayer = 'player1'
+        }
+
+        this.setState({
+            startPage: false,        
+            gameMap: [  
+                    ['','',''],
+                    ['','',''],
+                    ['','','']
+                ],
+            currentPlayer: curPlayer,
+            winner: false,
+            catGame: false,
+
+        })
+    }
+    
+
 
     render(){
 
-        const { startPage, gameMap, player1Name, player2Name, currentPlayer } = this.state;
+        const { startPage, gameMap, player1Name, player2Name, currentPlayer, winner, catGame } = this.state;
+
+        const catGamePage = (
+            <div className="winnerPage">
+                <p className='winnerPage__text'>Cat Game No Winner</p>
+                <button
+                onClick={this.restartHandler}>Play Again</button>
+            </div>
+        )
+        
+        const winnerPage = (
+            <div className="winnerPage">
+                <p className='winnerPage__text'>Congrats {currentPlayer}</p>
+                <button
+                onClick={this.restartHandler}>Play Again</button>
+            </div>
+        )
         
         const mainGame = (
             <Fragment>
                  <div className="tic__score">
+                 {winner ? winnerPage : catGame ? catGamePage : null}
                     <div className="tic__score-top" >
+                   
                         <div className="box">
                             <p 
                             className={gameMap[0][0]==='X' ? "text green" : "text red"}
-                            onClick={gameMap[0][0] ? null : ()=> this.addLetterHandler(0,0, currentPlayer)}
+                            onClick={gameMap[0][0] || winner ? null : ()=> this.addLetterHandler(0,0, currentPlayer)}
                             >{gameMap[0][0]}</p>
                         </div>
                         <div className="box">
                             <p 
                             className={gameMap[0][1]==='X' ? "text green" : "text red"}
-                            onClick={gameMap[0][1] ? null : ()=> this.addLetterHandler(0,1, currentPlayer)}>
+                            onClick={gameMap[0][1] || winner ? null : ()=> this.addLetterHandler(0,1, currentPlayer)}>
                             {gameMap[0][1]}</p>
                         </div>
                         <div className="box">
                             <p 
                             className={gameMap[0][2]==='X' ? "text green" : "text red"}
-                            onClick={gameMap[0][2] ? null : ()=> this.addLetterHandler(0,2, currentPlayer)}>
+                            onClick={gameMap[0][2] || winner ? null : ()=> this.addLetterHandler(0,2, currentPlayer)}>
                             {gameMap[0][2]}</p>
                         </div>
                         
@@ -96,19 +207,19 @@ class Tic extends Component {
                         <div className="box">
                             <p 
                             className={gameMap[1][0]==='X' ? "text green" : "text red"}
-                            onClick={gameMap[1][0] ? null : ()=> this.addLetterHandler(1,0, currentPlayer)}
+                            onClick={gameMap[1][0] || winner ? null : ()=> this.addLetterHandler(1,0, currentPlayer)}
                             >{gameMap[1][0]} </p>
                         </div>
                         <div className="box">
                             <p 
                             className={gameMap[1][1]==='X' ? "text green" : "text red"}
-                            onClick={gameMap[1][1] ? null : ()=> this.addLetterHandler(1,1, currentPlayer)}
+                            onClick={gameMap[1][1] || winner ? null : ()=> this.addLetterHandler(1,1, currentPlayer)}
                             >{gameMap[1][1]}</p>
                         </div>
                         <div className="box">
                             <p 
                             className={gameMap[1][2]==='X' ? "text green" : "text red"}
-                            onClick={gameMap[1][2] ? null : ()=> this.addLetterHandler(1,2, currentPlayer)}
+                            onClick={gameMap[1][2] || winner ? null : ()=> this.addLetterHandler(1,2, currentPlayer)}
                             >{gameMap[1][2]}</p>
                         </div>
                         
@@ -117,36 +228,37 @@ class Tic extends Component {
                         <div className="box">
                             <p  
                             className={gameMap[2][0]==='X' ? "text green" : "text red"}
-                            onClick={gameMap[2][0] ? null : ()=> this.addLetterHandler(2,0, currentPlayer)}
+                            onClick={gameMap[2][0] || winner ? null : ()=> this.addLetterHandler(2,0, currentPlayer)}
                             >{gameMap[2][0]}</p>
                         </div>
                         <div className="box">
                             <p  
                             className={gameMap[2][1]==='X' ? "text green" : "text red"}
-                            onClick={gameMap[2][1] ? null : ()=> this.addLetterHandler(2,1, currentPlayer)}
+                            onClick={gameMap[2][1] || winner ? null : ()=> this.addLetterHandler(2,1, currentPlayer)}
                             >{gameMap[2][1]}</p>
                         </div>
                         <div className="box">
                             <p  
                             className={gameMap[2][2]==='X' ? "text green" : "text red"}
-                            onClick={gameMap[2][2] ? null : ()=> this.addLetterHandler(2,2, currentPlayer)}
+                            onClick={gameMap[2][2] || winner ? null : ()=> this.addLetterHandler(2,2, currentPlayer)}
                             >{gameMap[2][2]}</p>
                         </div>                    
                     </div>
                 </div>
                 
                 <div className ="tic__players">
-                    <div className ="tic__players-1">
+                    <div className ={currentPlayer === 'player1' ? "tic__players-1 currentPlayer" : "tic__players-1"}>
                     {player1Name ? player1Name : 'Player 1'}
                     <p>X</p>
                     </div>
-                    <div className ="tic__players-2">
+                    <div className ={currentPlayer === 'player2' ? "tic__players-2 currentPlayer" : "tic__players-2"}>
                     {player2Name ? player2Name : "Player 2"}
                     <p>O</p>
                     </div>
 
 
                 </div>
+                
             </Fragment>
             
         )
@@ -179,8 +291,10 @@ class Tic extends Component {
                     </div>
                 </div>
             </Fragment>
-
         )
+
+
+        
 
             
             return(
